f9c27c2e6eb8353b328be85d344dd792bd98e6ae
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-03-03 12:47:58 +0000

    feat: replace a's with Link

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import logo from "../../logo_transparent.png";
 
 class Landing extends Component {
@@ -15,12 +17,12 @@ class Landing extends Component {
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
