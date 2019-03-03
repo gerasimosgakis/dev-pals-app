b1b4e0a01f2416160e6c7ea18f3e843f9c36f658
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-03-03 12:37:27 +0000

    feat: set up react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.scss";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -7,11 +9,13 @@ import Landing from "./components/layout/Landing";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
