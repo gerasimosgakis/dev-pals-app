db6b4a56f91a4e48319564a5684b28faf7941e9e
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-03-24 13:13:07 +0000

    cleanup: remove history and withRouter in Experience as we don't need it after all

--- src/components/dashboard/Experience.js
+++ src/components/dashboard/Experience.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
 import Moment from "react-moment";
 import { deleteExperience } from "../../actions/profileActions";
 
@@ -65,4 +64,4 @@ Experience.propTypes = {
 export default connect(
   null,
   { deleteExperience }
-)(withRouter(Experience));
+)(Experience);
