edfaa74109eac87c4e2543f75bfddf7517f4f90a
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-04-28 12:43:15 +0100

    fix: image size in landing page

--- src/components/profiles/ProfileItem.js
+++ src/components/profiles/ProfileItem.js
@@ -8,10 +8,14 @@ class ProfileItem extends Component {
     const { profile } = this.props;
 
     return (
-      <div className="card card-body bg-light mb-3">
+      <div className="card card-body bg-light mb-3 profile-item">
         <div className="row text-center">
-          <div className="col-lg-3 col-md-4 col-sm-5 col-12">
-            <img src={profile.avatar} alt="avatar" className="rounded-circle" />
+          <div className="col-lg-3 col-md-4 col-sm-5 col-12 profile-item__image-container">
+            <img
+              src={profile.avatar}
+              alt="avatar"
+              className="rounded-circle profile-item__image-container-image"
+            />
           </div>
           <div className="col-lg-4 col-md-4 col-sm-5 col-12">
             <h3>{profile.handle}</h3>
